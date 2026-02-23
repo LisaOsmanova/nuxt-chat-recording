3f92ff167d228b1aeece01cfc94dd77fc79763f5
LisaOsmanova <lischen.osmanova@gmail.com>
2026-02-24 00:06:52 +0200

    enhance server api

---
+++ app/pages/chat/[id].vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const route = useRoute();
+console.log(route.params);
+const { chat, messages, sendMessage } = useChat(route.params.id as string);
+const typing = ref(false);
+
+const handleSendMessage = async (message: string) => {
+  typing.value = true;
+  await sendMessage(message);
+  typing.value = false;
+};
+
+const appConfig = useAppConfig();
+const title = computed(() =>
+  chat.value?.title
+    ? `${chat.value.title} - ${appConfig.title}`
+    : appConfig.title,
+);
+
+useHead({
+  title,
+});
+</script>
+
+<template>
+  <ChatWindow :chat :messages :typing @send-message="handleSendMessage" />
+</template>
